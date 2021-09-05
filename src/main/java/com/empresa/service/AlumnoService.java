e26e41992a10f16b5fbb7b51d33e1a09ab0a7df1
LuisTV13 <luistorpoco13@gmail.com>
2021-09-04 21:33:29 -0500

    first commit

---
+++ src/main/java/com/empresa/service/AlumnoService.java
@@ -0,0 +1,10 @@
+package com.empresa.service;
+
+import java.util.List;
+
+import com.empresa.entity.Alumno;
+
+public interface AlumnoService {
+	public abstract Alumno insertaActualizaAlumno(Alumno obj);
+	public abstract List<Alumno> listaAlumno();
+}
